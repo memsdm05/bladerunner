a5d581af927b965cba430e06e2dc6968e1025d4f
Ben Browner <bnbrowner@gmail.com>
2022-04-24 22:49:46 -0400

    dump

--- manifest_compiler.py
+++ manifest_compiler.py
@@ -1,5 +1,7 @@
+from configparser import SectionProxy
 import sys
 import json
+import re
 
 if len(sys.argv) != 3:
     exit(1)
@@ -9,6 +11,9 @@ output = {
     "sections": [],
 }
 
+shots = output["shots"]
+sections = output["sections"]
+
 # parse shots
 with open(sys.argv[1], "r") as f:
     for line in f:
@@ -17,8 +22,7 @@ with open(sys.argv[1], "r") as f:
             continue
 
         parts = line.split(" ")
-        print(parts)
-        output["shots"].append({
+        shots.append({
             "frame": int(parts[0]),
             "distance": parts[1],
             "angle": {
@@ -29,11 +33,39 @@ with open(sys.argv[1], "r") as f:
             "special": len(parts) > 3
         })
 
-output["shots"].sort(key=lambda s: s["frame"])
+        print(shots[-1])
+
 
 # parse sections
+HEADER_PATTERN = re.compile(r"\((\d*)\) ([\w \']*)")
+with open(sys.argv[2], "r", encoding='utf-8') as f:
+    while True:
+        header = HEADER_PATTERN.match(f.readline())
+        if not header: continue
+        end, title = int(header.group(1)), header.group(2)
+
+        body = ""
+        while (body_line := f.readline()) != "\n":
+            body += body_line.strip() + "<br><br>"
+
+        sections.append({
+            "title": title,
+            "start": sections[-1]["end"] if len(sections) > 0 else 0,
+            "end": end,
+            "body": body
+        })
+
+        print(sections[-1])
+
+        if f.readline() == "":
+            break
+        
 
 
 # output
+shots.sort(key=lambda s: s["frame"])
+sections.sort(key=lambda s: s["end"])
 with open("manifest.json", "w") as f:
     json.dump(output, f, indent=2)
+
+print("\ndone")
